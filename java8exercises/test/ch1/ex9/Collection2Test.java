b1155aa1f98eb7967180581466a24ff485c6c57c
imikke <mikke.mr.children@gmail.com>
2015-06-15 12:07:10 +0900

    add ch9

--- java8exercises/test/ch1/ex9/Collection2Test.java
+++ java8exercises/test/ch1/ex9/Collection2Test.java
@@ -6,11 +6,16 @@ public class Collection2Test {
 
 	@Test
 	public void test() {
-		String addrs[] = { "NewYork", "Tokyo", "Osaka" };
+		String addrs[] = { "Yokohama", "NewYork", "Yokohama", "Osaka" };
 		ArrayListEx<String> lists = new ArrayListEx<>();
+		ArrayListEx<String> outlists = new ArrayListEx<>();
 		for (String addr : addrs)
 			lists.add(addr);
-		lists.forEachIf(string -> System.out.println(string),
-				string -> string != null);
+		lists.forEachIf(string -> {
+			// System.out.println(string);
+			// addをスレッドセーフで実行できるかは保証されない（ 1.6 変数スコープ参照）
+				outlists.add(string);
+			}, string -> string.equals("Yokohama"));
+		outlists.forEach(System.out::println);
 	}
 }
