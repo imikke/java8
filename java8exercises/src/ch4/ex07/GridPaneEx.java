42fd7a6d13e9710b21818a853e20b00ef83269e6
imikke <mikke.mr.children@gmail.com>
2015-08-12 16:28:31 +0900

    add ch4.ex07.

---
+++ java8exercises/src/ch4/ex07/GridPaneEx.java
@@ -0,0 +1,64 @@
+package ch4.ex07;
+
+import javafx.application.Application;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class GridPaneEx extends Application {
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		final double rem = new Text("").getLayoutBounds().getHeight();
+
+		GridPane pane = new GridPane();
+		// pane.setGridLinesVisible(true);
+
+		pane.setHgap(0.8 * rem);
+		pane.setVgap(0.8 * rem);
+		pane.setPadding(new Insets(0.8 * rem));
+		Label usernameLabel = new Label("User name:");
+		Label passwordLabel = new Label("Password:");
+		TextField username = new TextField();
+		PasswordField password = new PasswordField();
+
+		Button okButton = new Button("Ok");
+		Button cancelButton = new Button("Cancel");
+
+		HBox buttons = new HBox(0.8 * rem);
+		buttons.getChildren().addAll(okButton, cancelButton);
+		buttons.setAlignment(Pos.CENTER);
+		buttons.setBorder(new Border(new BorderStroke(Color.RED,
+				BorderStrokeStyle.SOLID, null, null)));
+		// buttons.setStyle("-fx-border-color: red;");
+
+		pane.add(usernameLabel, 0, 0);
+		pane.add(username, 1, 0);
+		pane.add(passwordLabel, 0, 1);
+		pane.add(password, 1, 1);
+		pane.add(buttons, 0, 2, 2, 1);
+
+		GridPane.setHalignment(usernameLabel, HPos.RIGHT);
+		GridPane.setHalignment(passwordLabel, HPos.RIGHT);
+		stage.setScene(new Scene(pane));
+		stage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
