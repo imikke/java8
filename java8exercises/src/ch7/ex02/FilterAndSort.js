caba4ae0c7ceefae158ab7421d11d18b147854e8
imikke <mikke.mr.children@gmail.com>
2015-09-27 13:36:53 +0900

    add ch7.ex01 and ch7.ex02.

---
+++ java8exercises/src/ch7/ex02/FilterAndSort.js
@@ -0,0 +1,17 @@
+var path = java.nio.file.Paths
+var file = java.nio.file.Files
+var bytes = file.readAllBytes(path.get('../../../test/words2.txt'))
+var contents = new java.lang.String(bytes, java.nio.charset.StandardCharsets.UTF_8)
+//contents
+var StringArray = Java.type('java.lang.String[]')
+var arrays = Java.to(contents.split(new RegExp("[\\W+]")), StringArray)
+var words = java.util.Arrays.asList(arrays)
+//words
+
+//重複を無くす
+var set = new java.util.HashSet(words)
+//set
+
+var stream = set.stream()
+var filter = stream.filter(function(w) {return w.length > 12})
+filter.sorted().forEach(function(w) { java.lang.System.out.println(w) })
